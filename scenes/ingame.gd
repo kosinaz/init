df367bc2c1888e3c2d61b519ccfb3e22a405651d
kosinaz <kosinaz@gmail.com>
2023-08-03 23:31:02 +0200

    add collision

--- scenes/ingame.gd
+++ scenes/ingame.gd
@@ -12,16 +12,60 @@ func _process(delta):
 
 
 func _on_up_pressed():
+	var map_position = $%TileMap.local_to_map($%Agent.position)
+	var tile = $%TileMap.get_cell_atlas_coords(0, Vector2i(map_position))
+	if tile.y == 4:
+		if tile.x == 2: return
+		if tile.x == 3: return
+		if tile.x == 5: return
+		if tile.x == 6: return
+		if tile.x == 7: return
+	if tile.y == 5:
+		if tile.x == 0: return
+		if tile.x == 2: return
 	$%Agent.position.y -= 6
 
 
 func _on_right_pressed():
+	var map_position = $%TileMap.local_to_map($%Agent.position)
+	var tile = $%TileMap.get_cell_atlas_coords(0, Vector2i(map_position))
+	if tile.y == 4:
+		if tile.x == 2: return
+		if tile.x == 3: return
+		if tile.x == 7: return
+	if tile.y == 5:
+		if tile.x == 1: return
+		if tile.x == 3: return
+		if tile.x == 5: return
+		if tile.x == 7: return
 	$%Agent.position.x += 6
 
 
 func _on_down_pressed():
+	var map_position = $%TileMap.local_to_map($%Agent.position)
+	var tile = $%TileMap.get_cell_atlas_coords(0, Vector2i(map_position))
+	if tile.y == 4:
+		if tile.x == 3: return
+	if tile.y == 5:
+		if tile.x == 0: return
+		if tile.x == 2: return
+		if tile.x == 3: return
+		if tile.x == 4: return
+		if tile.x == 6: return
+		if tile.x == 7: return
 	$%Agent.position.y += 6
 
 
 func _on_left_pressed():
+	var map_position = $%TileMap.local_to_map($%Agent.position)
+	var tile = $%TileMap.get_cell_atlas_coords(0, Vector2i(map_position))
+	if tile.y == 4:
+		if tile.x == 2: return
+		if tile.x == 4: return
+		if tile.x == 6: return
+	if tile.y == 5:
+		if tile.x == 1: return
+		if tile.x == 2: return
+		if tile.x == 3: return
+		if tile.x == 6: return
 	$%Agent.position.x -= 6
