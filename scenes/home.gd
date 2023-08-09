014905d15c75a0a13b25dbff12fd06ec35b2fa13
kosinaz <kosinaz@gmail.com>
2023-08-09 22:11:06 +0200

    add level 1 title and intro

--- scenes/home.gd
+++ scenes/home.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _on_button_pressed():
-	add_sibling(load("res://scenes/ingame.tscn").instantiate())
+	add_sibling(load("res://scenes/level1title.tscn").instantiate())
 	queue_free()
